297b8c7c10cfaa8ab4092dcba853e0a13df2a678
Dmitry Khruslov <dem6n@yandex.ru>
2022-05-15 21:21:36 +0300

    add some fix

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -9,53 +9,37 @@ public class FelineTest {
 
     @Test
     public void eatMeatReturnsCorrectValue() throws Exception {
-        // Arrange
+
         Feline feline = new Feline();
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-
-        // Act
         List<String> actualFood = feline.eatMeat();
-
-        // Assert
         Assert.assertEquals(expectedFood, actualFood);
     }
 
     @Test
     public void getFamilyReturnsCorrectValue() {
-        // Arrange
+
         Feline feline = new Feline();
         String expectedFamily = "Кошачьи";
-
-        // Act
         String actualFamily = feline.getFamily();
-
-        // Assert
         Assert.assertEquals(expectedFamily, actualFamily);
     }
 
     @Test
     public void getKittensDefaultConstructorReturnsCorrectValue() {
-        // Arrange
+
         Feline feline = new Feline();
         int expectedKittensCount = 1;
-
-        // Act
         int actualKittensCount = feline.getKittens();
-
-        // Assert
         Assert.assertEquals(expectedKittensCount, actualKittensCount);
     }
 
     @Test
     public void getKittensExtendedConstructorReturnsCorrectValue() {
-        // Arrange
+
         Feline feline = new Feline();
         int expectedKittensCount = 3;
-
-        // Act
         int actualKittensCount = feline.getKittens(3);
-
-        // Assert
         Assert.assertEquals(expectedKittensCount, actualKittensCount);
     }
 }
