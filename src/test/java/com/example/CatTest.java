dc3375cb3c7b9260a9acec60569e017ae4d0c2e9
Dmitry Khruslov <dem6n@yandex.ru>
2022-05-29 20:57:57 +0300

    cat test done and check with mutation

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,10 +2,18 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+
+    @Mock
+    Feline feline;
 
     @Test
     public void getSoundReturnsCorrectValue() {
@@ -19,7 +27,8 @@ public class CatTest {
     @Test
     public void getFoodReturnsCorrectValue() throws Exception {
 
-        Cat cat = new Cat(new Feline());
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         List<String> actualFood = cat.getFood();
         Assert.assertEquals(expectedFood, actualFood);
